edb223b4c22e932065d63c24c484bda85675985c
claireL00 <20191316@s.ubaguio.edu>
2022-05-27 23:10:29 +0800

    Shop Sprite with Animation

--- js/classes.js
+++ js/classes.js
@@ -1,19 +1,43 @@
 class Sprite{
-    constructor({position,imageSrc}){
+    constructor({position,imageSrc,scale=1,framesMax=1}){
         this.position=position
         this.width=50
         this.height=150
         this.image=new Image()
         this.image.src=imageSrc
+        this.scale=scale
+        this.framesMax=framesMax
+        this.framesCurrent=0
+        this.framesElapsed =0
+        this.framesHold= 5//how many frames to hold b4 it goes to the actual animation 
     }
 
     draw(){
-        c.drawImage(this.image,this.position.x, this.position.y)
+        c.drawImage(
+            this.image,
+            this.framesCurrent*(this.image.width /this.framesMax),
+            0,
+            this.image.width / this.framesMax, //divide the image by 6 for the 6 shops (number of frames)
+            this.image.height,
+            this.position.x, 
+            this.position.y,
+            (this.image.width /this.framesMax) *this.scale,
+            this.image.height * this.scale
+            )
     }
    
     //moving of object
     update(){
     this.draw()
+    this.framesElapsed++
+    if(this.framesElapsed % this.framesHold === 0){
+        if(this.framesCurrent<this.framesMax-1){
+            this.framesCurrent++
+        }else{
+            this.framesCurrent=0
+        }
+    }  
+    
     }
 
 }
